e67684f5a4233067d53be5d85d73cd3ebcdbfc95
Dante <leontin91andrei@yahoo.com>
2019-11-26 21:47:26 +0200

    backup of my project

---
+++ Clicker/Assets/Scenes/Clasa2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clasa2 : MonoBehaviour
+{
+    public int var1 = 5;
+    public static int var2 = 2;
+
+    public void MetodaMea()
+    {
+        int rezultat = var1 + var2;
+        Debug.Log("MyRez: " + rezultat);
+    }
+
+    public static void MetodaStat()
+    {
+        int var3 = 15;
+        int rez = var2 + var3;
+        Debug.Log("Rezultat static: " + rez);
+    }
+}
---
+++ Clicker/Assets/Scenes/Mate1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mate1 : MonoBehaviour
+{
+    int myVar = 10;
+    Clasa2 myClass = new Clasa2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rez = myVar + myClass.var1;
+        Debug.Log("Rezultat 1: " + rez + " pe obiectul " + gameObject.name);
+        //myClass.MetodaMea();
+
+        rez = myVar + Clasa2.var2;
+        Debug.Log("Rezultat 2: " + rez + " pe obiectul " + gameObject.name);
+        //Clasa2.MetodaStat();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
